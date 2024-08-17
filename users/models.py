c2fbcefcb8823edbfbfc2d98b6a4ee93070caade
Вячеслав Лукин <bafana.slv@gmail.com>
2024-08-17 17:45:00 +0300

    Сериализаторы: задание № 1

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from courses.models import Courses, Lessons
 
 NULLABLE = {"blank": True, "null": True}
 
@@ -22,3 +23,26 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payments(models.Model):
+    METHOD = [('cache', 'наличными'),
+              ('transfer', 'перевод')]
+
+    user = models.ForeignKey(User, related_name='users', on_delete=models.PROTECT, verbose_name="пользователь")
+    payment_date = models.DateField(auto_now_add=True, verbose_name="дата оплаты")
+    paid_course = models.ForeignKey(Courses, related_name='paid_course', on_delete=models.PROTECT, verbose_name="оплаченный курс", **NULLABLE)
+    paid_lesson = models.ForeignKey(Lessons, related_name='paid_lesson', on_delete=models.PROTECT, verbose_name="оплаченный урок", **NULLABLE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="cумма оплаты")
+    payment_method = models.CharField(max_length=10, choices=METHOD, verbose_name="способ оплаты")
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+
+    def str(self):
+        if self.paid_course is not None:
+            course_or_lesson = 'за курс'
+        else:
+            course_or_lesson = 'за урок'
+        return f'Оплата за {course_or_lesson}, дата оплаты: {self.payment_date}, сумма: {self.payment_method}, способ: {self.payment_method}'
